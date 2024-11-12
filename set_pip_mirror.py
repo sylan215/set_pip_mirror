938e4ba82f89b99386a5bd1460bd8d1a26d2cd84
sylan215 <sylan2158@gmail.com>
2024-11-12 21:16:09 +0800

    init
    功能实现文件

---
+++ set_pip_mirror.py
@@ -0,0 +1,61 @@
+﻿# -*- coding: utf-8 -*-
+
+"""
+工具说明：一键设置 python 镜像，跨平台支持
+author：sylan215
+"""
+
+import platform
+import os
+
+
+def write_to_file(filename):
+    # 统一写入镜像配置信息
+    with open(filename, 'w') as f_op:
+        f_op.write('[global]\n')
+        f_op.write('index-url = https://pypi.tuna.tsinghua.edu.cn/simple\n')
+        f_op.write('[install]\n')
+        f_op.write('trusted-host=http://mirrors.aliyun.com\n')
+        f_op.write('\n')
+
+
+def set_windows_mirror():
+    # 设置 windows 环境的 python 镜像
+    username = os.getlogin()
+    pip_dir = 'C:\\Users\\' + username + '\\pip'
+    if not os.path.isdir(pip_dir):
+        os.mkdir(pip_dir)
+    pip_inf = pip_dir + '\\pip.ini'
+    write_to_file(pip_inf)
+
+
+def set_linux_mirror():
+    # 设置 linux 环境的 python 镜像
+    username = os.getlogin()
+    pip_dir = '/home/' + username + '/.pip'
+    if not os.path.isdir(pip_dir):
+        os.mkdir(pip_dir)
+    pip_inf = pip_dir + '/pip.conf'
+    write_to_file(pip_inf)
+
+
+def check_os():
+    # 获取操作系统类型
+    os_name = platform.system()
+    
+    if os_name == "Windows":
+        return 1
+    elif os_name == "Linux":
+        return 2
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    os_type = check_os()
+    if os_type == 1:
+        set_windows_mirror()
+    elif os_type == 2:
+        set_linux_mirror()
+
+
